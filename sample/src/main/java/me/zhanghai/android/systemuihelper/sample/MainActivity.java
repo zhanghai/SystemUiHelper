70f05b02bfa2bab48b2e8d6dcff5557e94dcafcd
Zhang Hai <dreaming.in.code.zh@gmail.com>
2015-11-30 19:51:13 +0800

    Implement sample.

--- sample/src/main/java/me/zhanghai/android/systemuihelper/sample/MainActivity.java
+++ sample/src/main/java/me/zhanghai/android/systemuihelper/sample/MainActivity.java
@@ -5,12 +5,32 @@
 
 package me.zhanghai.android.systemuihelper.sample;
 
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
+import butterknife.Bind;
+import butterknife.BindInt;
 import butterknife.ButterKnife;
+import me.zhanghai.android.systemuihelper.SystemUiHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindInt(android.R.integer.config_mediumAnimTime)
+    int mToolbarHideDuration;
+
+    @Bind(R.id.appbar_wrapper)
+    LinearLayout mAppbarWrapperLayout;
+    @Bind(R.id.appbar)
+    FrameLayout mAppbarLayout;
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+
+    private SystemUiHelper mSystemUiHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +38,40 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.main_activity);
         ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+
+        mSystemUiHelper = new SystemUiHelper(this, SystemUiHelper.LEVEL_IMMERSIVE,
+                SystemUiHelper.FLAG_IMMERSIVE_STICKY,
+                new SystemUiHelper.OnVisibilityChangeListener() {
+                    @Override
+                    public void onVisibilityChange(boolean visible) {
+                        if (visible) {
+                            mAppbarWrapperLayout.animate()
+                                    .alpha(1)
+                                    .translationY(0)
+                                    .setDuration(mToolbarHideDuration)
+                                    .setInterpolator(new FastOutSlowInInterpolator())
+                                    .start();
+                        } else {
+                            mAppbarWrapperLayout.animate()
+                                    .alpha(0)
+                                    .translationY(-mAppbarLayout.getBottom())
+                                    .setDuration(mToolbarHideDuration)
+                                    .setInterpolator(new FastOutSlowInInterpolator())
+                                    .start();
+                        }
+                    }
+                });
+        // This will set up window flags.
+        mSystemUiHelper.show();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+            mSystemUiHelper.toggle();
+        }
+        return super.onTouchEvent(event);
     }
 }
